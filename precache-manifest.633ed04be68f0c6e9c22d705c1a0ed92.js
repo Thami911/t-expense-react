0087a73b4d02e9746bfae2f47723a884dbd486f9
Thami911 <thamimncwabe@gmail.com>
2024-03-28 13:29:55 +0200

    Updates

---
+++ precache-manifest.633ed04be68f0c6e9c22d705c1a0ed92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2834f82759d665e779b4d8121c9dc20",
+    "url": "/t-expense-react/index.html"
+  },
+  {
+    "revision": "ab60a1f0fc0d87b4af1e",
+    "url": "/t-expense-react/static/css/main.69a4c9b2.chunk.css"
+  },
+  {
+    "revision": "7a29eb9db402bccdffbb",
+    "url": "/t-expense-react/static/js/2.1261af49.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/t-expense-react/static/js/2.1261af49.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ab60a1f0fc0d87b4af1e",
+    "url": "/t-expense-react/static/js/main.c9ebef6c.chunk.js"
+  },
+  {
+    "revision": "1dfe1e777654efb642e2",
+    "url": "/t-expense-react/static/js/runtime-main.9091829a.js"
+  }
+]);
